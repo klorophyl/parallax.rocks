d0350cee8ef4378703c6a079867848852279040f
Gabriel Samain <gabriel.samain@gmail.com>
2017-11-18 17:19:53 +0100

    Add players and linkgs

--- js/oktogon.js
+++ js/oktogon.js
@@ -1,3 +1,17 @@
+// Boot
+
+var isDesktop = $(window).width() > 1000;
+
+$(".banner").animate({"opacity": 1}, 1000);
+
+if (!isDesktop) {
+    $("#spotify").width($(window).width());
+    $("#deezer").width($(window).width());
+}
+
+
+// Animations
+
 function drawOktogon(id, color, angle) {
     var canvas = document.getElementById(id);
     var ctx = canvas.getContext("2d");
@@ -43,15 +57,17 @@ function animateOpacity(id, opacity) {
     $("#" + id).animate({"opacity": opacity}, 40);
 }
 
-function blink(id) {
-   animateOpacity(id, "0");
+function blink(id, count=0) {
+    count += 1;
+
+    animateOpacity(id, "0");
     setTimeout(function() {
        animateOpacity(id, "1");
     }, Math.floor(Math.random() * 50) + 50);
 
-    if (Math.random() > 0.4) {
+    if (Math.random() > 0.4 && count < 4) {
         setTimeout(function() {
-            blink(id);
+            blink(id, count);
         }, Math.floor(Math.random() * 50) + 50);
     }
 }
@@ -63,24 +79,31 @@ function blinkName() {
 }
 
 function init() {
-    $("#oktogon-black").animate({"opacity": 1}, 0);
-    $("#oktogon-white").animate({"opacity": 1}, 1000, blinkName);
-    $("#album-name").animate({"opacity": 1}, 1000);
+    if (isDesktop) {
+        $("#oktogon-black").animate({"opacity": 1}, 0);
+        $("#oktogon-white").animate({"opacity": 1}, 1000, blinkName);
+        $("#album-name").animate({"opacity": 1}, 1000);
+        $(".left").addClass("left-animate");
+        $(".right").addClass("right-animate");
+    }
+
+    $(".buttons").animate({"opacity": 1}, 1000);
 }
 
 $(window).bind("load", function() {
+    if (isDesktop) {
+        $("#oktogon-white").attr("width", 0.3 * $(window).width() + "px");
+        $("#oktogon-white").attr("height", 0.3 * $(window).width() + "px");
 
-    $("#oktogon-white").attr("width", 0.38 * $(window).width() + "px");
-    $("#oktogon-white").attr("height", 0.38 * $(window).width() + "px");
+        $("#oktogon-black").attr("width", 0.279 * $(window).width() + "px");
+        $("#oktogon-black").attr("height", 0.279 * $(window).width() + "px");
 
-    $("#oktogon-black").attr("width", 0.35 * $(window).width() + "px");
-    $("#oktogon-black").attr("height", 0.35 * $(window).width() + "px");
+        drawOktogon("oktogon-white", "white", Math.PI / 8);
+        drawOktogon("oktogon-black", "black", 0);
 
-    drawOktogon("oktogon-white", "white", Math.PI / 8);
-    drawOktogon("oktogon-black", "black", 0);
-
-    rotateOktogon("oktogon-black", 0, Math.PI / 8);
-    rotateOktogon("oktogon-white", 0, -Math.PI / 8);
+        rotateOktogon("oktogon-black", 0, Math.PI / 8);
+        rotateOktogon("oktogon-white", 0, -Math.PI / 8);
+    }
 
     setTimeout(init, 300);
 });
